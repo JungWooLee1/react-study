a90573498c93fe7e6d0657fa32e1b37d6bc374b3
Jungwoo Lee <jjjjjw910911@letsee.io>
2018-09-07 19:01:45 +0900

    update

---
+++ src/components/search_var_backup.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+
+const SearchBar = () => {
+    return <input /> // JSX => html // React.createElement
+}
+
+const foo = 5;
+
+
+export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from  './components/search_var';
+
+const API_KEY = 'AIzaSyAIuHU7Cs9_xBcbw6FJ5CQTCYxRxdyMOAA';
 
 // Create a new component. This component shold produce
 // some HTML
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (<div>
+        <SearchBar/>
+    </div>
+    );
 };
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-
 
