2fd409cc3045708c073decb49b60b718d0b12cf2
Jungwoo Lee <jjjjjw910911@letsee.io>
2018-09-17 18:44:30 +0900

    27, 28, 29 강좌 적용

--- src/index.js
+++ src/index.js
@@ -3,31 +3,45 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from  './components/search_var';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
+
 const API_KEY = 'AIzaSyAIuHU7Cs9_xBcbw6FJ5CQTCYxRxdyMOAA';
 
 // Create a new component. This component shold produce
 // some HTML
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
-            console.log(videos);
-        })
-    }
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
 
-    render() {
-        return (
-            <div>
-                <SearchBar/><br/>
-                <VideoList videos={this.state.videos}/>
-            </div>
-        )
-    }
+      console.log(videos);
+      console.log('API 리턴 완료 setState 호출');
+      this.setState({
+        videos:videos,
+        selectedVideo:videos[0]
+      });
+    })
+  }
+  
+  render() {
+    console.log('메인 render() 호출');
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it
